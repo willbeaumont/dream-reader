fc0ef494572620186f72e4a111fa00a16618e79e
Will Beaumont <39424606+willbeaumont@users.noreply.github.com>
2025-01-18 19:40:00 -0800

    fix loader render

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -15,51 +15,54 @@ import { DreamCard } from "../components/dream-card";
 
 Amplify.configure(outputs);
 
-export const History = () => {
+const Content = () => {
   const [data, setData] = useState<Schema["Dream"]["type"][]>();
   const [isLoading, setIsLoading] = useState(true);
   const { user } = useAuthenticator((context) => [context.user]);
 
   useEffect(() => {
-    try {
-      if (user) {
-        setIsLoading(true);
-        client.models.Dream.list().then((response) => {
-          setData(
-            response.data.sort((a, b) => {
-              const d1 = new Date(a.createdAt).getTime();
-              const d2 = new Date(b.createdAt).getTime();
-              return d2 - d1;
-            })
-          );
-        });
-      }
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsLoading(false);
+    const fetchDreams = async () => {
+      const { data } = await client.models.Dream.list();
+      setData(
+        data.sort((a, b) => {
+          const d1 = new Date(a.createdAt).getTime();
+          const d2 = new Date(b.createdAt).getTime();
+          return d2 - d1;
+        })
+      );
+    };
+
+    if (user) {
+      setIsLoading(true);
+      fetchDreams().then(() => setIsLoading(false));
     }
   }, [user]);
 
   if (!user) return <Text>Please sign in</Text>;
   if (isLoading) return <Loader />;
-  if (!data?.length) return <Text>No dreams found</Text>;
+  if (!data?.length) return <Text>No dreams found, navigate to "Dream"</Text>;
+  return (
+    <Collection
+      items={data}
+      type="list"
+      direction="row"
+      gap="20px"
+      wrap="nowrap"
+      isPaginated
+      itemsPerPage={3}
+    >
+      {(item, index) => <DreamCard data={item} index={index} />}
+    </Collection>
+  );
+};
+
+export const History = () => {
   return (
     <View>
       <Heading level={2} marginBottom={15}>
         History
       </Heading>
-      <Collection
-        items={data}
-        type="list"
-        direction="row"
-        gap="20px"
-        wrap="nowrap"
-        isPaginated
-        itemsPerPage={3}
-      >
-        {(item, index) => <DreamCard data={item} index={index} />}
-      </Collection>
+      <Content />
     </View>
   );
 };
